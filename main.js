716d92e35dbcaecbed3fdb2ef9269bb97c90f5b5
Igor Shimanskyi <shima3d@gmail.com>
2019-04-09 22:11:21 +0300

    Внешние скрипты с async и defer. Console.log и вывод в нее переменных, констант, чисел

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 let dog = "Jack";
-dog = "jons";
+let cat = "Jons";
+let homeNumber = 12;
+const digit = 5;
+const side = "left";
+var flag = 2;
+var counter =1000;
+
+
+alert("the " + dog + " and " + cat + " living in the home at number " + homeNumber + " more of " + digit +" years " )
+console.log(dog+cat);
+console.log(homeNumber-digit);
+console.log(side-digit);
+console.log(side+digit);
+console.log(counter/flag+digit);
